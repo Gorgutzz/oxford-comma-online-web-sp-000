97ec02374271617e03218d7185cb796c11a38ae9
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-01 21:17:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   if array.length == 1
     return array
   elsif array.length == 2
-    return array.join(" ' ", " ' ")
+    return array.join(" and ")
+  elsif array.length == 3
+    return array.join(', ') + ", and "
 
 end
